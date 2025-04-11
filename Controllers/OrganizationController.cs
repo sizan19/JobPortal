7943402f84277533acd7e2f99f3d97ee3f57e4e6
sizan <sizan@DESKTOP-UDG0225>
2025-04-11 08:31:50 +0545

    Adding 2 method as http post and get method on controller

--- Controllers/OrganizationController.cs
+++ Controllers/OrganizationController.cs
@@ -8,5 +8,43 @@ namespace JobPortal.Controllers
         {
             return View();
         }
+
+        //If empty , it will be a GET request
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            return View();
+        }
     }
 }
