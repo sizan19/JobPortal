c498aa2b4ebb4552a32194ae8ffb3cad7cdf3a6b
sizan <sizan@DESKTOP-UDG0225>
2025-04-15 08:40:44 +0545

    Feathing entity model to view model by creating a new object for view model.

--- Controllers/OrganizationController.cs
+++ Controllers/OrganizationController.cs
@@ -17,8 +17,19 @@ namespace JobPortal.Controllers
         public IActionResult Index()
         {
 
-
+            OrganizationVM model = new OrganizationVM();
+            model.OrganizationList = (from i in _db.Organization    //SElECT * FROM Organization
+                                      select new OrganizationVM
+                                      {
+                                          OrganizationId = i.OrganizationId,   //converting from Entitymdoel to viewmodel
+                                          OrgName = i.OrgName,
+                                          OrgAddress = i.OrgAddress,
+                                          OrgContact = i.OrgContact,
+                                          OrgEmail = i.OrgEmail,
+                                      }).ToList();
             return View();
+
+            
         }
 
         //If empty , it will be a GET request
