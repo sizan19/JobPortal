d66383d3c2913dd33991f72cd2de99d48b2abb10
sizan <sizan@DESKTOP-UDG0225>
2025-04-22 08:02:18 +0545

    Added function for image upload on it so that image is uploaded whereas we create it for create Edit .

--- Controllers/OrganizationController.cs
+++ Controllers/OrganizationController.cs
@@ -1,11 +1,16 @@
 ﻿using JobPortal.Data;
 using JobPortal.Models.EntityModels;
 using JobPortal.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace JobPortal.Controllers
+
+
 {
+    [Authorize]
     public class OrganizationController : Controller
     {
 
@@ -28,6 +33,7 @@ namespace JobPortal.Controllers
                                           OrgAddress = i.OrgAddress,
                                           OrgContact = i.OrgContact,
                                           OrgEmail = i.OrgEmail,
+                                          OrgImage = i.OrgImage,
                                       }).ToList();
             return View(model);
 
@@ -43,15 +49,30 @@ namespace JobPortal.Controllers
 
         [HttpPost]
 
-        public IActionResult Create(OrganizationVM model)
+        public IActionResult Create(OrganizationVM model, IFormFile? file)
         {
+            if (file != null)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                string fileNameWithPath = Path.Combine(path, file.FileName);
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                model.OrgImage = $"/Files/{file.FileName}";
+            }
+
+            var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             Organization entityorg = new Organization();
             entityorg.OrgName = model.OrgName;
             entityorg.OrgAddress = model.OrgAddress;
             entityorg.OrgContact = model.OrgContact;
             entityorg.OrgEmail = model.OrgEmail;
-            entityorg.CreatedBy = "Admin"; //Set the created by field
+            entityorg.CreatedBy = UserId; //Set the created by field
             entityorg.CreatedDate = DateTime.Now; //Set the created date field
+            entityorg.OrgImage = model.OrgImage;    
             _db.Entry(entityorg).State = EntityState.Added; //Add the entity to the database
             _db.SaveChanges(); //Save the changes to the database
             return RedirectToAction("Index");
@@ -71,7 +92,8 @@ namespace JobPortal.Controllers
                 OrgName = data.OrgName,
                 OrgAddress = data.OrgAddress,
                 OrgContact = data.OrgContact,
-                OrgEmail = data.OrgEmail
+                OrgEmail = data.OrgEmail,
+                OrgImage = data.OrgImage
             };
 
             return View(model);
@@ -79,19 +101,41 @@ namespace JobPortal.Controllers
 
         [HttpPost]
 
-        public IActionResult Edit(OrganizationVM model)
+        public IActionResult Edit(OrganizationVM model, IFormFile? file)
         {
+
+            if (file != null)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                string fileNameWithPath = Path.Combine(path, file.FileName);
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                model.OrgImage = $"/Files/{file.FileName}";
+            }
+            var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
             Organization entityorg = new Organization();
             entityorg.OrganizationId = model.OrganizationId;
             entityorg.OrgName = model.OrgName;
             entityorg.OrgAddress = model.OrgAddress;
             entityorg.OrgContact = model.OrgContact;
             entityorg.OrgEmail = model.OrgEmail;
-            entityorg.UpdatedBy = "Admin"; //Set the created by field
+            entityorg.OrgImage = model.OrgImage;
+            entityorg.UpdatedBy = UserId; //Set the created by field
             entityorg.UpdatedDate = DateTime.Now; //Set the created date field
             _db.Entry(entityorg).State = EntityState.Modified; //Add the entity to the database
             _db.Entry(entityorg).Property(x => x.CreatedBy).IsModified = false;  // so that these values were removed when updating database
             _db.Entry(entityorg).Property(x => x.CreatedDate).IsModified = false;
+
+            if(file == null)
+            {
+                _db.Entry(entityorg).Property(x => x.OrgImage).IsModified = false; // so that these values were removed when updating database
+
+            }
 
             _db.SaveChanges(); //Save the changes to the database
             return RedirectToAction("Index");
