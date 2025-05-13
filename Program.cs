b06a9ab1026b96f99104332b2e365bfb570a994e
Sizzy <84314549+sizan19@users.noreply.github.com>
2025-05-13 21:37:50 +0545

    fix for update

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios
     app.UseHsts();
 }
 
@@ -32,7 +32,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}") 
     .WithStaticAssets();
 
 app.MapRazorPages();
