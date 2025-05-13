cd8aa1c4a93bc92b2625105c742c1eda07543d03
sizan <sizan@DESKTOP-UDG0225>
2025-05-13 12:24:32 +0545

    send these changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using System.Security.Claims;
+using JobPortal.Data;
 using JobPortal.Models;
+using JobPortal.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobPortal.Controllers
@@ -8,20 +10,29 @@ namespace JobPortal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly JobPortalContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, JobPortalContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var userName = User.FindFirstValue(ClaimTypes.Name);
-                ViewBag.UserName = userName; // Pass username to ViewBag
-            }
-            return View();
+            OrganizationVM model = new OrganizationVM();
+            model.OrganizationList = (from i in _db.Organization    //SElECT * FROM Organization
+                                      where i.DeltetedDate == null
+                                      select new OrganizationVM
+                                      {
+                                          OrganizationId = i.OrganizationId,   //converting from Entitymdoel to viewmodel
+                                          OrgName = i.OrgName,
+                                          OrgAddress = i.OrgAddress,
+                                          OrgContact = i.OrgContact,
+                                          OrgEmail = i.OrgEmail,
+                                          OrgImage = i.OrgImage,
+                                      }).ToList();
+            return View(model);
         }
 
         public IActionResult Privacy()
--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages();
